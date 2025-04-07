84bd18ed44513e88cc0d401213a8986723ec84ab
dogged <uzoechijerry@gmail.com>
2025-04-07 03:09:13 +0100

    about

--- src/components/SigninModal.tsx
+++ src/components/SigninModal.tsx
@@ -29,15 +29,24 @@ export default function SigninModal({
         {/* <DialogTrigger>Open</DialogTrigger> */}
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
+            <DialogTitle className=" mb-20 text-2xl font-bold font-popins text-center">
+              Get Started
+            </DialogTitle>
+            <DialogDescription className="  text-lg font-montserrat">
               This action cannot be undone. This will permanently delete your
               account and remove your data from our servers.
             </DialogDescription>
           </DialogHeader>
-          <DialogFooter>
-            <Button onClick={() => signIn("google")} type="submit">
-              Confirm
+          <DialogFooter className=" mt-30">
+            <Button
+              variant={"default"}
+              className="w-full font-popins text-base font-medium  "
+              size={"lg"}
+              onClick={() => signIn("google")}
+              type="submit"
+            >
+              <img src="/Google2x.png" alt="image" className=" mx-2" />
+              Singin Up with google
             </Button>
           </DialogFooter>
         </DialogContent>
